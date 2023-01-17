049a3e49ca8daa92cb27a2b9e0b0e1100984d493
game035 <game035@mclab.local>
2023-01-17 13:34:02 -0800

    File test

---
+++ Assets/Scripts/Weapons/FireStaff.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEngine.Localization
+{
+    public class FireStaff : MonoBehaviour
+    {
+        public GameObject basicPrimary;
+        public GameObject basicSecondary;
+        public GameObject basicFlourish;
+
+        public GameObject specialPrimary;
+
+        //Player pressed LMB
+        public void BasicAttack()
+        {
+
+        }
+        //Player pressed RMB
+        public void SpecialAttack()
+        {
+
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
